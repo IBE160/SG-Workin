3d5f80817fe3477acc620d17c5cd31505cbb7f63
oewean <orjan@wean.no>
2025-12-15 21:51:29 +0100

    vercel fix1

--- backend/api/index.py
+++ backend/api/index.py
@@ -4,4 +4,4 @@ import os
 # Add the project root to sys.path to allow imports from backend package
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
-from main import app
+from backend.main import app
